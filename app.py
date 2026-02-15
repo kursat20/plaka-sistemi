0a0a0e09295ade1d683d28541aa5e319870386c7
kursat20 <kursatcomert55@gmail.com>
2026-02-15 15:53:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -71,10 +71,7 @@ def sil(plaka):
 
 if __name__ == "__main__":
     os.makedirs("static/ceza_fotolari", exist_ok=True)
-    app.run(debug=True)
-
-import os
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
